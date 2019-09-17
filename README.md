56c53ec3be536a3f77ed1f2219a999e6da10791b
Herman Platou <hsplatou@gmail.com>
2019-09-17 22:16:39 +0200

    work in progress

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Stacc Code Challenge
+ 
+ Pseudo:
+ #Pseudo method - draw final matrix
+#Draw matrix from dictionary of positions, but before writing the dictionary key (this would be the number) draw an X if the position is present in the marking array
--- Solver/solver.py
+++ Solver/solver.py
@@ -1,40 +1,53 @@
 import numpy as np
+import pprint
 
+gMarkingArr=[]
 
-#Create random matrix.
-#Specify the size and the range (range is inclusive)
+# Create random matrix.
+# Specify the size and the range (range is inclusive)
 def createRandomMatrix(pSize, pToInt):
-    return  np.random.randint(pToInt+1, size=(pSize, pSize))
+    return np.random.randint(pToInt+1, size=(pSize, pSize))
 
 
-#create specific matrix
-#content of matrix in format: '1 1 1 1 1 ; 2 2 2 2 2 ; 3 3 3 3 3'
+# create specific matrix
+# content of matrix in format: '1 1 1 1 1 ; 2 2 2 2 2 ; 3 3 3 3 3'
 def createSpecificMatrix(*pNumbers):
     return np.matrix(*pNumbers)
-    #handle exception
+    # handle exception
 
-  
- 
-#decode matrix into formal format
+
+# decode matrix into formal format
 def decodeMatrix(pMatrix):
-        vMatrixPositions={}
-        vCurrIndexRow = 0
+    vMatrixPositions = {}
+    vCurrIndexRow = 0
+    vCurrIndexElem = 0
+    for row in pMatrix:
+        for element in row:
+            if element not in vMatrixPositions.keys():  # create new element
+                vMatrixPositions[element] = [[vCurrIndexElem, vCurrIndexRow]]
+            else:
+                # get array, append new entry to array
+                vMatrixPositions[element].append(
+                    [vCurrIndexElem, vCurrIndexRow])
+            vCurrIndexElem += 1
         vCurrIndexElem = 0
-        for row in pMatrix:
-                vCurrIndexRow+=1
-                for element in row:
-                        vCurrIndexElem+=1
-                        if element not in vMatrixPositions.keys(): #create new element
-                                vMatrixPositions[element] = 300
-                                
+        vCurrIndexRow += 1
+    return vMatrixPositions
 
-                         
+def removeHorizontalDuplicates(pDict):
+        for arr in pDict.values():
+                print(arr)
+
 
  
 
-                 
-
+####MAIN####
+#create matrix
 vTemp = createRandomMatrix(10, 10)
-print(vTemp)
-
  
+#decode matrix into dictionary
+vDictionary = decodeMatrix(vTemp)
+pprint.pprint(vDictionary)
+
+#remove horizontal duplicates
+removeHorizontalDuplicates(vDictionary)
