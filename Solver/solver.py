16153988dfd2ac65d76e6fc0731102f39df31aee
Herman Platou <hsplatou@gmail.com>
2019-09-18 19:53:15 +0200

    implementing horizontal

--- Solver/solver.py
+++ Solver/solver.py
@@ -1,7 +1,7 @@
 import numpy as np
 import pprint
 
-gMarkingArr = {}
+gMarkingDict = {}
 
 # Create random matrix.
 # Specify the size and the range (range is inclusive)
@@ -40,9 +40,22 @@ def decodeMatrix(pMatrix):
     return vMatrixPositions
 
 # goes through each array and checks for duplicates, in effect marking down any duplicates that exist on the same row
+#increases int such that we do not seach unneccesarily
 def markHorizontalDuplicates(pDict):
-        for arr in pDict.values():
-                for element in arr:
+          for i in range(len(pDict)):
+                vCurrArr = pDict[i]
+                skip = 0
+                for element in vCurrArr:
+                        yPosition = element[1] 
+                        for e in vCurrArr[skip:]:                                
+                                if yPosition == e[1]:
+                                        addArrToDictionary(gMarkingDict, i, element)
+                        skip+=1
+
+
+
+#[[5, 2], [9, 2], [6, 6], [0, 7], [5, 8], [7, 9]],
+
 
 
 
@@ -50,11 +63,89 @@ def markHorizontalDuplicates(pDict):
 
 ####MAIN####
 # create matrix
-vTemp = createRandomMatrix(10, 10)
- 
+#vTemp = createRandomMatrix(10, 10)
+#print(vTemp)
 # decode matrix into dictionary
-vDictionary = decodeMatrix(vTemp)
-pprint.pprint(vDictionary)
-
+vDictionary = {0: [[4, 0],
+     [5, 0],
+     [6, 0],
+     [5, 1],
+     [1, 3],
+     [4, 4],
+     [4, 5],
+     [1, 6],
+     [0, 7],
+     [6, 7],
+     [5, 8],
+     [9, 8],
+     [1, 9],
+     [8, 9],
+     [9, 9]],
+ 1: [[3, 2], [6, 3], [9, 4], [5, 5], [6, 5], [0, 8]],
+ 2: [[8, 0],
+     [0, 1],
+     [3, 1],
+     [3, 4],
+     [2, 6],
+     [2, 7],
+     [5, 7],
+     [3, 8],
+     [8, 8],
+     [2, 9],
+     [3, 9]],
+ 3: [[2, 0],
+     [6, 1],
+     [1, 2],
+     [4, 2],
+     [5, 2],
+     [2, 3],
+     [5, 4],
+     [8, 4],
+     [6, 8],
+     [6, 9]],
+ 4: [[1, 0], [2, 1], [9, 5], [8, 7], [2, 8], [7, 8]],
+ 5: [[7, 1], [8, 1], [2, 4], [4, 6], [7, 6], [7, 7], [1, 8], [7, 9]],
+ 6: [[4, 1], [3, 3], [1, 7], [0, 9], [4, 9], [5, 9]],
+ 7: [[9, 0],
+     [6, 2],
+     [7, 2],
+     [9, 2],
+     [8, 3],
+     [9, 3],
+     [1, 4],
+     [2, 5],
+     [3, 5],
+     [9, 6]],
+ 8: [[3, 0],
+     [7, 0],
+     [9, 1],
+     [0, 2],
+     [7, 3],
+     [0, 5],
+     [7, 5],
+     [5, 6],
+     [6, 6],
+     [4, 8]],
+ 9: [[0, 0], [8, 2], [0, 4], [0, 6], [3, 6], [4, 7], [9, 7]],
+ 10: [[1, 1],
+      [2, 2],
+      [0, 3],
+      [4, 3],
+      [5, 3],
+      [6, 4],
+      [7, 4],
+      [1, 5],
+      [8, 5],
+      [8, 6],
+      [3, 7]]}
+  
 # remove horizontal duplicates
 markHorizontalDuplicates(vDictionary)
+pprint.pprint(vDictionary)
+print('separator')
+pprint.pprint(gMarkingDict)
+
+
+ 
+         
+ 
